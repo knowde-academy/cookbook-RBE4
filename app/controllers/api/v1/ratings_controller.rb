2ad813c02f39b908730b1acf1de84ab001387b6f
Szymon Biel <szymek.2k@gmail.com>
2022-07-15 08:30:58 +0000

    Remove unnecessary rescue

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class RatingsController < ApplicationController
-      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
    
       def create
         @recipe = Recipe.find(params[:recipe_id])
@@ -19,10 +18,7 @@ module Api
       def rating_params
         params.require(:rating).permit(%i[author rating])
       end
-      
-      def record_not_found
-        render json: { errors: "rating not found" }
-      end
+    
     end
   end
 end
