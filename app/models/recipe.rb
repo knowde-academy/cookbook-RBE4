8b5b009a81df4b6457d4a178c4e2fd7b2bb7d633
Maciej Biel <maciek.b2k@gmail.com>
2022-07-16 16:26:54 +0000

    Remove unnecessary validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,19 +4,8 @@ class Recipe < ApplicationRecord
   validates :price, numericality: { greater_than: 0.0, less_than: 10**6 }, allow_nil: true
   validates :time, numericality: { in: 0..10080 }, allow_nil: true
   
-  # without it you can pass e.g. 1.12345 and it's casted and rounded by default
-  validate :price_before_cast
-  
   has_many :comments, lambda { order(created_at: :desc) }
   has_many :ratings, lambda { order(created_at: :desc) }
   has_many :product_recipes, dependent: :destroy
   has_many :products, through: :product_recipes
-  
-  private
-  def price_before_cast
-    # Regex is only matched against strings
-    unless price_before_type_cast.to_s =~ /\A\d+(?:\.\d{0,2})?\z/
-      errors.add('price', 'Price must match the correct format.')
-    end
-  end
 end
